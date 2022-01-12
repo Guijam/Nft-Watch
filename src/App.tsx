9695054486f627e569e7dc6ce1f84a3cf8fbddaf
GuillaumeJamin <guillaume.jamin@aptea.net>
2022-01-12 22:32:23 +0100

    Logos2

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App: React.FC = () => {
         </span>
           <a href="https://nftkey.app/" target="_blank" rel="noreferrer"><img src="./img/nftkey-logo.svg" alt="logo" className="Nftkey-logo" /></a>
           <br/><br/>
+
       </header>
       <EventPrinter />
     </div>
